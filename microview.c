4d53525f1ab1cd0578a4286c8b1b988ced93f4bf
HypersonicKitsune <hypersonic.kitsune@gmail.com>
2018-06-26 11:29:14 +0200

    Microview now loads and display pictures (yay!)
    Doing File -> Open displays a dialog box which let you load the picture and displays it.
    /!\ Uses GtkStock stuff which is deprecated, it must be replaced ASAP

--- microview.c
+++ microview.c
@@ -15,6 +15,53 @@ GtkBuilder *builder     = NULL;
 GError     *error       = NULL;
 gchar      *gui_file    = NULL;
 
+
+
+/* static void on_open_image(GtkImageMenuItem *menuopen, gpointer user_data)
+ * 
+ * Called when clicking File -> Open; creates a dialog and loads images
+ * TODO 1: Replace GtkStock with non-deprecated stuff!!!
+ * TODO 2: Move it to an other file?
+ */
+
+static void on_open_image(GtkImageMenuItem *menuopen, gpointer user_data)
+{
+    GtkWidget *image = GTK_WIDGET (user_data);
+    GtkWidget *toplevel = gtk_widget_get_toplevel (image);
+    
+	GtkFileFilter *filter = gtk_file_filter_new ();
+	GtkWidget *dialog = gtk_file_chooser_dialog_new (("Open image"),
+	                                                 GTK_WINDOW (toplevel),
+	                                                 GTK_FILE_CHOOSER_ACTION_OPEN,
+	                                                 GTK_STOCK_OK, GTK_RESPONSE_ACCEPT,
+	                                                 GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+	                                                 NULL);
+
+	gtk_file_filter_add_pixbuf_formats (filter);
+    
+	gtk_file_filter_add_pixbuf_formats (filter);
+	gtk_file_chooser_add_filter (GTK_FILE_CHOOSER (dialog),
+	                             filter);
+
+	switch (gtk_dialog_run (GTK_DIALOG (dialog)))
+	{
+		case GTK_RESPONSE_ACCEPT:
+		{
+			gchar *filename =
+				gtk_file_chooser_get_filename (GTK_FILE_CHOOSER (dialog));
+			gtk_image_set_from_file (GTK_IMAGE (image), filename);
+			break;
+		}
+		default:
+			break;
+	}
+	gtk_widget_destroy (dialog);
+}
+
+
+
+/*** MAIN ***/
+
 int main(int argc, char *argv[])
 {	
 	// setting up GTK
@@ -42,6 +89,7 @@ int main(int argc, char *argv[])
 
 	// setting up the main window...
 	g_signal_connect(G_OBJECT(main_window), "destroy", (GCallback)gtk_main_quit, NULL);
+    g_signal_connect(G_OBJECT(menuopen), "activate", G_CALLBACK(on_open_image), image);
     g_signal_connect(G_OBJECT(menuquit), "activate", (GCallback)gtk_main_quit, NULL);
 	gtk_widget_show_all(main_window);
 
